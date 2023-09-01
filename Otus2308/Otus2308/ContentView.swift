dc13b4d962c9b0ba5229c8b90b37cf2e91c48158
sv_galagan <galagan_sv@magnit.ru>
2023-09-01 11:39:45 +0300

    init commit

---
+++ Otus2308/Otus2308/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Otus2308
+//
+//  Created by Galagan Sergey on 01.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Otus2308/Otus2308/Otus2308App.swift
@@ -0,0 +1,17 @@
+//
+//  Otus2308App.swift
+//  Otus2308
+//
+//  Created by Galagan Sergey on 01.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Otus2308App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
